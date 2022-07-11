f63c9aade631ab620fd4725eab22b4fc094f283b
Zeason <20301138@bjtu.edu.cn>
2022-07-11 22:35:30 +0800

    114514等无关紧要的修改

--- src/fpconnect/fpconnect.controller.ts
+++ src/fpconnect/fpconnect.controller.ts
@@ -3,18 +3,22 @@ import { FpconnectService } from './fpconnect.service';
 
 @Controller('fpconnect')
 export class FpconnectController {
-    constructor(private readonly fpservice: FpconnectService){}
+  constructor(private readonly fpservice: FpconnectService) { }
 
-    @Post()
-    fin(@Body() body){
-      return this.fpservice.createdb(body);
-    }
-    @Post('getAllLocal')
-    getLocal(@Body() body){
-      return this.fpservice.getAllLocation();
-    }
-    @Post('getALocal')
-    getALocal(@Body() body){
-      return this.fpservice.getalocation(body)
-    }
+  @Post()
+  fin(@Body() body) {
+    return this.fpservice.createdb(body);
+  }
+  @Post('getall-local')
+  getLocal(@Body() body) {
+    return this.fpservice.getAllLocation();
+  }
+  @Post('getalocal')
+  getALocal(@Body() body) {
+    return this.fpservice.getalocation(body)
+  }
+  @Get()
+  thri() {
+    return '1145141919810\n逸一时误一世，逸久逸久罢已凌'
+  }
 }
