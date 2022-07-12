59e81cd7b2242ae7d75cdb4bbb631510ce1ee436
Yi Ming <ofseed@foxmail.com>
2022-07-12 19:54:11 +0800

    启用Swagger模块

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Inoor Positioning Server')
+    .setDescription('Inoor Positioning Server API')
+    .setVersion('0.1.0')
+    .addTag('inoor-positioning-server')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -11,6 +22,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
   await app.listen(3000);
 }
 bootstrap();
