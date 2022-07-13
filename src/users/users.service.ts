04fb2d469fa3ce4ed5de96d033cf4a939d2d9a7f
Yi Ming <ofseed@foxmail.com>
2022-07-13 11:30:40 +0800

    启用对User的ORM

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,6 @@
 import { ConflictException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 
@@ -16,6 +18,11 @@ export class UsersService {
       password: 'ruangoushigou',
     },
   ];
+
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
 
   async findOne(username: string): Promise<User | undefined> {
     return this.users.find((user) => user.username === username);
